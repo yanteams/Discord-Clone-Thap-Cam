4247dd6553625f8486393e198ea8d0354d691cf1
YanCoder <khoant20@uef.edu.vn>
2023-11-07 13:31:46 +0700

    Navigation sidebar

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -18,7 +18,7 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
         <Image fill src={value} alt="Upload" className="rounded-full" />
         <button
           onClick={() => onChange("")}
-          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm "
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
