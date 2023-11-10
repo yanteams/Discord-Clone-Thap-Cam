31e6d90b0400586fb543de0fb19611eb6b0ac2f6
YanCoder <khoant20@uef.edu.vn>
2023-11-11 05:00:39 +0700

    Complete Messsage API Socket

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,5 +1,7 @@
 "use client";
 import * as z from "zod";
+import axios from "axios";
+import qs from "query-string";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Plus, Smile } from "lucide-react";
@@ -22,8 +24,16 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
     },
   });
   const isLoading = form.formState.isSubmitting;
-  const onSubmit = async (value: z.infer<typeof formSchema>) => {
-    console.log(value);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: apiUrl,
+        query,
+      });
+      await axios.post(url, values);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <Form {...form}>
